f082dddfd6656843c572a6ee91b6acfd211d6efb
vishnusankoti-personal <vishnuvardhan.sankoti@gmail.com>
2023-02-14 16:39:25 -0600

    ok

---
+++ code/EDI.Utilities/EDI.Utilities/EDI.Utilities/FileHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDI.Utilities
+{
+    public static class FileHelper
+    {
+        public static bool CopyFiles(string sourceFilePath, string destinationFolderPath)
+        {
+            bool success = false;
+            try
+            {
+                string inputFileName = RetrieveFile(sourceFilePath);
+                File.Copy(sourceFilePath, $"{destinationFolderPath}{inputFileName}", true);
+
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+            success = true;
+            return success;
+        }
+
+
+        private static string RetrieveFile(string filePath)
+        {
+            string fileName = string.Empty;
+            string[] filePathArray = filePath.Split('\\');
+            fileName = filePathArray[filePathArray.Length - 1];
+            return fileName;
+        }
+    }
+}
---
+++ code/EDI.Utilities/EDIUtilitiesHelper.Console/EDIUtilitiesHelper.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using EDI.Utilities;
+using System;
+
+namespace EDIUtilitiesHelper.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool success = FileHelper.CopyFiles(@"C:\Vishnu\SourceFolder\test.txt", @"C:\Vishnu\DesitnationFolder\HR\");
+
+            }catch(Exception ex)
+            {
+                string s = ex.Message;
+            }
+        }
+    }
+}
